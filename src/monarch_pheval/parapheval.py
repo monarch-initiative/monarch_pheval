ab3bf05e50d44c37d042f8a31cc369f36b915e8e
souzadevinicius <souzadevinicius@gmail.com>
2024-11-12 14:45:13 +0000

    fix: yaml and makefiles must be separetad also per corpusvariant and corpusid

--- src/monarch_pheval/parapheval.py
+++ src/monarch_pheval/parapheval.py
@@ -5,8 +5,9 @@ import os
 import shutil
 import subprocess
 import time
+from functools import partial
 from pathlib import Path
-from typing import List
+from typing import Any, Dict, List
 
 import yaml
 
@@ -55,20 +56,20 @@ def check_last_job_running():
     return
 
 
-def submit_job(id: str, template: Path, submitted_file: Path):
+def submit_job(id_corpus: str, template: Path, submitted_file: Path):
     """
     submit job to HPC cluster.
 
     Args:
-        id (str): pheval run id
+        id_corpus (str): pheval run id and corpus id
         template (Path): job template file
         submitted_file (Path): name of file that will be created after job submission. it is useful to check.
         if this job have been submited before. when force flag is true this check will be ignored
 
     """
-    shutil.copy(f"./jobs/{id}.Makefile", "./Makefile")
+    shutil.copy(f"./jobs/{id_corpus}.Makefile", "./Makefile")
     cmd = f"sbatch {template}"
-    logging.info(f"running {id}")
+    logging.info(f"running {id_corpus}")
     result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
     if result.returncode != 0:
         logging.error(f"Error running sbatch: {result.stderr}")
@@ -90,50 +91,57 @@ def write_conf(filename: str, data: List):
         yaml.dump(data, file, default_flow_style=False)
 
 
-def filter_data(section: List, key: str, value: str):
+def filter_data(section: List[Dict[str, Any]], key: str, value: str) -> List[Dict[str, Any]]:
     """
-    filter configuration in PhEval file based on run id.
+    Filter configuration in PhEval file based on a key-value pair.
 
     Args:
-        section (List): _description_
-        key (str): _description_
-        value (str): _description_
+        section (List[Dict[str, Any]]): List of dictionaries to filter
+        key (str): The key to filter on
+        value (str): The value to match
 
     Returns:
-        _type_: _description_
+        List[Dict[str, Any]]: Filtered list of dictionaries
 
     """
-    return [d for d in section if d[key] == value]
+    return [d for d in section if d.get(key) == value]
 
 
-def split_runs(data: List, id: str):
+def split_runs(data: Dict[str, Any], run: Dict[str, str]) -> None:
     """
-    split runs in separeted makefiles.
+    Split runs into separate YAML files.
 
     Args:
-        data (List): Parsed PhEval-Config Yaml data
-        id (str): Run id
+        data (Dict[str, Any]): Parsed PhEval-Config YAML data
+        run (Dict[str, str]): Run data containing configuration, corpus, and corpusvariant
 
     """
+    config_id = run["configuration"]
     copied_data = data.copy()
-    copied_data["configs"] = filter_data(data["configs"], "id", id)
-    copied_data["runs"] = filter_data(data["runs"], "configuration", id)
-    write_conf(filename=f"{id}.yaml", data=copied_data)
+
+    filter_func = partial(filter_data, key="id", value=config_id)
+    copied_data["configs"] = filter_func(data["configs"])
+
+    copied_data["runs"] = data["runs"]
+    for key, value in run.items():
+        copied_data["runs"] = filter_data(copied_data["runs"], key, value)
+
+    write_conf(filename=f"{config_id}-{run['corpus']}-{run['corpusvariant']}.yaml", data=copied_data)
 
 
-def generate_makefile(id: str = None):
+def generate_makefile(id_corpus: str = None):
     """
-    generate make file based on run id.
+    generate make file based on run configuration id and corpus id.
 
     Args:
-        id (str, optional): Run ID present on pheval-config.yaml file.
+        id_corpus (str, optional): Run ID + Run Corpus forming an ID string present on pheval-config.yaml file.
         Defaults to None.
 
     """
     os.makedirs("./jobs", exist_ok=True)
-    config = "./resources/pheval-config.yaml" if id is None else f"./jobs/{id}.yaml"
-    output = "./jobs/original.Makefile" if id is None else f"./jobs/{id}.Makefile"
-    logging.info(f'Generating {"original" if id is None else id} Makefile')
+    config = "./resources/pheval-config.yaml" if id_corpus is None else f"./jobs/{id_corpus}.yaml"
+    output = "./jobs/original.Makefile" if id_corpus is None else f"./jobs/{id_corpus}.Makefile"
+    logging.info(f'Generating {"original" if id_corpus is None else id_corpus} Makefile to {output}')
     return subprocess.run(
         [
             "./resources/generatemakefile.sh",
@@ -143,7 +151,7 @@ def generate_makefile(id: str = None):
         ],
         capture_output=True,
         text=True,
-        shell=True,
+        shell=False,
     )
 
 
@@ -161,25 +169,26 @@ def execute_job(template_job: Path, max_jobs: int = -1, force: bool = False):
     logging.info("starting")
     with open("./resources/pheval-config.yaml", "r") as file:
         data = yaml.safe_load(file)
-        # generate_makefile()
-        for c in data["runs"]:
-            id = c["configuration"]
-            corpus = c["corpus"]
-            corpus_variant = c["corpusvariant"]
+        generate_makefile()
+        for run in data["runs"]:
+            configuration_id = run["configuration"]
+            corpus = run["corpus"]
+            corpus_variant = run["corpusvariant"]
+            id_corpus = f"{configuration_id}-{corpus}-{corpus_variant}"
             jobs = get_user_jobs()
             if len(jobs) >= max_jobs:
                 logging.info("max jobs submitted")
                 return
-            submitted_file = f"./jobs/{id}-{corpus}-{corpus_variant}.submitted"
+            submitted_file = f"./jobs/{id_corpus}.submitted"
             if os.path.exists(submitted_file) and not force:
-                # logging.info(f"{id} job is already submitted")
+                # logging.info(f"{id_corpus} job is already submitted")
                 continue
-            split_runs(data, id)
-            generate_makefile(id)
+            split_runs(data, run)
+            generate_makefile(id_corpus=id_corpus)
             check_last_job_running()
-            submit_job(id=id, template=template_job, submitted_file=submitted_file)
-            # backing to the original makefile
-            # shutil.copy('./jobs/original.Makefile', './Makefile')
+            submit_job(id_corpus=id_corpus, template=template_job, submitted_file=submitted_file)
             logging.info("waiting job start (2 minutes)")
             time.sleep(120)
+    # backing to the original makefile
+    shutil.copy("./jobs/original.Makefile", "./Makefile")
     logging.info("done")
